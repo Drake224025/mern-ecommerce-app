e165a8541f22fea4fcf4b4666efbfeea066a9903
Drake224025 <sasithamare@gmail.com>
2024-04-11 23:40:41 +0530

    refactor: reset css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css"; // Your existing CSS (if any)
-import "@picocss/pico"; // Import Pico.css
 import "./App.css"; // Your custom CSS
 
 ReactDOM.createRoot(document.getElementById("root")).render(
